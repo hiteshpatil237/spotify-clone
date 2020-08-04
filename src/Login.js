21fc59380ca442d9ae3d4ad42c61e75b347a4cae
hiteshpatil237 <hiteshpatil237@gmail.com>
2020-08-04 19:19:50 +0530

    added spotify logo

---
+++ src/Login.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Login.css';
+
+function Login() {
+    return (
+        <div className="login">
+            <img src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg" alt="Logo" />
+        </div>
+    )
+}
+
+export default Login;
