dfc564141109daa25ced6682a501fced1e118591
hiteshpatil237 <hiteshpatil237@gmail.com>
2020-08-05 19:09:44 +0530

    connected Spotify to the app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,41 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import Login from './Login';
+import { getTokenFromUrl } from './spotify';
+import SpotifyWebApi from 'spotify-web-api-js';
 import './App.css';
 
+const spotify = new SpotifyWebApi();
+
 function App() {
+  const [token, setToken] = useState(null);
+
+  useEffect(() => {
+    const hash = getTokenFromUrl();
+
+    window.location.hash = "";
+
+    const _token = hash.access_token;
+    if(_token) {
+      setToken(_token);
+
+      spotify.setAccessToken(_token);
+      spotify.getMe().then(user => {
+        console.log("🧑", user);
+      });
+    }
+
+    console.log("I have a token 💁‍♂️ ", token);
+  }, []);
   return (
     <div className="app">
-      <Login />
+      {
+        token ? (
+          <h1>Song Player</h1>
+        ) : (
+          <Login />
+        )
+      }
     </div>
   );
 }
