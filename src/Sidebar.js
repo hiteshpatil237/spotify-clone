dbacf08f8108e6df7a93a08a91eb8d3b14024a59
hiteshpatil237 <hiteshpatil237@gmail.com>
2020-08-07 19:31:34 +0530

    added sidebar partial

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,5 +1,9 @@
 import React from 'react'
 import './Sidebar.css';
+import SidebarOption from './SidebarOption';
+import HomeIcon from '@material-ui/icons/Home';
+import SearchIcon from '@material-ui/icons/Search';
+import LibraryMusicIcon from '@material-ui/icons/LibraryMusic';
 
 function Sidebar() {
     return (
@@ -9,6 +13,17 @@ function Sidebar() {
                 src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg" 
                 alt="spotify" 
             />
+            <SidebarOption Icon={HomeIcon} title="Home" />
+            <SidebarOption Icon={SearchIcon} title="Search" />
+            <SidebarOption Icon={LibraryMusicIcon} title="Your Library" />
+
+            <br />
+            <strong className="sidebar__title">PLAYLISTS</strong>
+            <hr />
+
+            <SidebarOption title="Hip Hop" />
+            <SidebarOption title="Rock" />
+            <SidebarOption title="Pop" />
         </div>
     )
 }
