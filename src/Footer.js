7af62803f39a4f6c27e1696b336f3b23f570213b
hiteshpatil237 <hiteshpatil237@gmail.com>
2020-08-08 12:50:15 +0530

    completed the footer player design

--- src/Footer.js
+++ src/Footer.js
@@ -1,10 +1,49 @@
 import React from 'react';
 import './Footer.css';
+import PlayCircleOutlineIcon from '@material-ui/icons/PlayCircleOutline';
+import SkipPreviousIcon from '@material-ui/icons/SkipPrevious';
+import SkipNextIcon from '@material-ui/icons/SkipNext';
+import ShuffleIcon from '@material-ui/icons/Shuffle';
+import RepeatIcon from '@material-ui/icons/Repeat';
+import VolumeDownIcon from "@material-ui/icons/VolumeDown";
+import PauseCircleOutlineIcon from "@material-ui/icons/PauseCircleOutline";
+import PlaylistPlayIcon from "@material-ui/icons/PlaylistPlay";
+import { Grid, Slider } from "@material-ui/core";
 
 function Footer() {
     return (
         <div className="footer">
-            <h1>Footer</h1>
+            <div className="footer__left">
+                <img 
+                    className="footer__albumLogo" 
+                    src="https://t2.genius.com/unsafe/275x0/https%3A%2F%2Fimages.genius.com%2F0036a50b5ac42635ccbf1eb13b89db27.1000x1000x1.jpg" 
+                    alt="Album Image" 
+                />
+                <div className="footer__songInfo">
+                    <h4>Mas Queso</h4>
+                    <p>Floyd Wonder</p>
+                </div>
+            </div>
+            <div className="footer__center">
+                <ShuffleIcon className="footer__green" />
+                <SkipPreviousIcon className="footer__icon" />
+                <PlayCircleOutlineIcon fontSize="large" className="footer__icon" />
+                <SkipNextIcon className="footer__icon" />
+                <RepeatIcon className="footer__green" />
+            </div>
+            <div className="footer__right">
+                <Grid container spacing={2}>
+                    <Grid item>
+                        <PlaylistPlayIcon />
+                    </Grid>
+                    <Grid item>
+                        <VolumeDownIcon />
+                    </Grid>
+                    <Grid item xs>
+                        <Slider  />
+                    </Grid>
+                </Grid>
+            </div>
         </div>
     )
 }
--- src/reducer.js
+++ src/reducer.js
@@ -3,7 +3,7 @@ export const initialState = {
     playlists: [],
     playing: false,
     item: null,
-    //token: "BQB8cdGR5uSWOHyAA8BTDxqsePvPXq3kWs6foYlb0aPNSlm_WYKxFWlcmBamvO5wwqbBFDVXx_npXqHDFJGO4eZWHbMNuVjbpl7FXeA-3aCqoles_d-2rRC2xw-Kdz7BkBYnLrZt84AY9sV8qv9fuume8RFNLXwwtoTrbrguSM9GKjV-"
+    token: "BQB8cdGR5uSWOHyAA8BTDxqsePvPXq3kWs6foYlb0aPNSlm_WYKxFWlcmBamvO5wwqbBFDVXx_npXqHDFJGO4eZWHbMNuVjbpl7FXeA-3aCqoles_d-2rRC2xw-Kdz7BkBYnLrZt84AY9sV8qv9fuume8RFNLXwwtoTrbrguSM9GKjV-"
 };
 
 const reducer = (state, action) => {
